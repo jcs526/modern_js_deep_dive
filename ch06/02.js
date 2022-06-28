91ba3a892e55ddb0c04dd08871fef59fbe805715
ChanSol <mutongzang@gmail.com>
2022-06-28 23:43:15 +0900

    28일자 학습

---
+++ ch06/02.js
@@ -0,0 +1,9 @@
+var binary = 0b01000001; // 2진수
+var octal = 0o101; // 8진수
+var hex = 0x41; // 16진수
+
+console.log(binary); // 65
+console.log(octal); // 65
+console.log(hex); // 65
+console.log(binary === octal); // true
+console.log(octal === hex); // true
---
+++ ch06/05.js
@@ -0,0 +1,2 @@
+// 자바 스크립트는 대소문자를 구별한다.
+var x = nan; // ReferenceError : nan is not defined
